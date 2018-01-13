135b5382dd28595e43dbfd6c0e0fcd1068cebd05
Timmo Freudl-Gierke <timmo.freudl-gierke@meteogroup.com>
2018-01-13 20:37:12 +0100

    presets loaded and printed

--- arduino-openhab-sonos-controller.ino
+++ arduino-openhab-sonos-controller.ino
@@ -1,114 +1,202 @@
-/*
-   UIPEthernet TcpClient example.
-
-   UIPEthernet is a TCP/IP stack that can be used with a enc28j60 based
-   Ethernet-shield.
-
-   UIPEthernet uses the fine uIP stack by Adam Dunkels <adam@sics.se>
-
-        -----------------
-
-   This TcpClient example gets its local ip-address via dhcp and sets
-   up a tcp socket-connection to 192.168.0.1 port 5000 every 5 Seconds.
-   After sending a message it waits for a response. After receiving the
-   response the client disconnects and tries to reconnect after 5 seconds.
-
-   Copyright (C) 2013 by Norbert Truchsess <norbert.truchsess@t-online.de>
-*/
-
 #include <UIPEthernet.h>
+#include <HttpClient.h>
 
-EthernetClient client;
-signed long tcpTimeout;
+EthernetClient c;
+HttpClient http(c);
+byte mac[6] = {0x00, 0x01, 0x02, 0x03, 0x03, 0x07};
+const char kHostname[] = "Timmos-MacBook.de.meteogroup.net";
+const char kPath[] = "/rest/items/SonosPresets/state";
+const int kNetworkTimeout = 5 * 1000;
+const int kNetworkDelay = 1000;
 
-uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05};
+// #include <SPI.h>
+// #include <Wire.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SSD1306.h>
+// #define OLED_RESET 4
+// Adafruit_SSD1306 display(OLED_RESET);
+// #if (SSD1306_LCDHEIGHT != 64)
+// #error("Height incorrect, please fix Adafruit_SSD1306.h!");
+// #endif
 
-// Destination host and port
-const char dsthost[] = "10.49.8.193";
-IPAddress dstip = IPAddress(10, 49, 8, 193);
-const uint16_t dstport = 8080;
+#define BUF_SIZE 100
+char presets[BUF_SIZE];
+int presetsLength = 0;
+int numPresets = 0;
 
-String presets;
-
-
-void setup() {
-
+void setup()
+{
   Serial.begin(9600);
+  Serial.println(F("Welcome"));
 
-  Serial.println("Welcome");
-
-  while (Ethernet.begin(mac) != 1)
-  {
-    Serial.println("Error getting IP address via DHCP, trying again...");
-    delay(5000);
-  }
-  Serial.println("Ethnert initialized");
-
-  Serial.print("localIP: ");
-  Serial.println(Ethernet.localIP());
-  Serial.print("subnetMask: ");
-  Serial.println(Ethernet.subnetMask());
-  Serial.print("gatewayIP: ");
-  Serial.println(Ethernet.gatewayIP());
-  Serial.print("dnsServerIP: ");
-  Serial.println(Ethernet.dnsServerIP());
-
-  tcpTimeout = 0;
+  // initializeDisplay();
+  initializeEthernet();
+  // displayIpAddress();
 
   loadPresets();
+  tokenizePresets();
+  printoutPresets();
 
+  // displayPresets();
 }
 
-void loop() {
-
-  delay(5000);
-}
-
-void loadPresets() {
-  tcpTimeout = millis() + 5000;
-
-  Serial.println("Loading Presets ....");
-  if (client.connect(dstip, dstport))
+void initializeEthernet()
+{
+  delay(1000);
+  while (Ethernet.begin(mac) != 1)
   {
-    Serial.print("Connected to ");
-    Serial.println(dsthost);
-
-    // send request
-
-    char getbuf[96] = {0};
-    char hostbuf[32] = {0};
-    sprintf(getbuf, "GET /rest/items/SonosPresets/state");
-    sprintf(hostbuf, "\n Host: %s\n\n", dsthost);
-    client.print(getbuf);
-    client.print(hostbuf);
-    Serial.println("Request Sent");
-
-    // response handling
-
-//    client.println("DATA from Client: ");
-    while (client.available() == 0)
-    {
-      if (tcpTimeout - millis() < 0)
-        goto stop;
-    }
-    Serial.println("Response received");
-    int size;
-    while ((size = client.available()) > 0)
-    {
-      char* data = (char*)malloc(size + 1);
-      size = client.read(data, size);
-      data[size] = '\0';
-      presets = String(data);
-      free(data);
-    }
-stop:
-    client.stop();
-    Serial.print("Presets loaded: ");
-    Serial.println(presets);
+    Serial.println(F("Error getting IP address via DHCP, trying again..."));
+    delay(3000);
   }
-  else {
-    Serial.println("Connection failed!");
-  }
-
+  Serial.print(F("localIP: "));
+  Serial.println(Ethernet.localIP());
+  // Serial.print("subnetMask: ");
+  // Serial.println(Ethernet.subnetMask());
+  // Serial.print("gatewayIP: ");
+  // Serial.println(Ethernet.gatewayIP());
+  // Serial.print("dnsServerIP: ");
+  // Serial.println(Ethernet.dnsServerIP());
 }
 
+void loadPresets()
+{
+  int err = 0;
+  err = http.get(kHostname, 8080, kPath);
+  if (err != 0)
+  {
+    Serial.print(F("Cannot connect to "));
+    Serial.println(err);
+  }
+  else
+  {
+    Serial.println(F("startedRequest ok"));
+
+    int statusCode = http.responseStatusCode();
+    if (statusCode < 0)
+    {
+      Serial.print(F("Getting response failed: "));
+      Serial.println(err);
+    }
+    else
+    {
+      Serial.print(F("Got status code: "));
+      Serial.println(statusCode);
+
+      err = http.skipResponseHeaders();
+      if (err >= 0)
+      {
+        unsigned long timeoutStart = millis();
+        char c;
+        presetsLength = 0;
+        // Whilst we haven't timed out & haven't reached the end of the body
+        while ((http.connected() || http.available()) &&
+               ((millis() - timeoutStart) < kNetworkTimeout))
+        {
+          if (http.available())
+          {
+            presets[presetsLength++] = http.read();
+            // We read something, reset the timeout counter
+            timeoutStart = millis();
+          }
+          else
+          {
+            // We haven't got any data, so let's pause to allow some to
+            // arrive
+            delay(kNetworkDelay);
+          }
+        }
+        presets[presetsLength] = 0;
+      }
+      else
+      {
+        Serial.print(F("Failed to skip response headers: "));
+        Serial.println(err);
+      }
+    }
+  }
+  http.stop();
+}
+
+void tokenizePresets()
+{
+  // Serial.println("Tokenize Presets:");
+  if (presets > 0)
+    numPresets = 1;
+  else
+    numPresets = 0;
+  for (int i = 0; i < presetsLength; i++)
+  {
+    if (presets[i] == ',')
+    {
+      presets[i] = 0;
+      numPresets++;
+    }
+  }
+}
+
+char *getPreset(int p)
+{
+  int token = 0;
+  for (int i = 0; i < presetsLength; i++)
+  {
+    if (token == p)
+      return &presets[i];
+    if (presets[i] == 0)
+      token++;
+  }
+  return nullptr;
+}
+
+void printoutPresets()
+{
+  Serial.println("Presets:");
+  for (int i = 0; i < numPresets; i++)
+  {
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.println(getPreset(i));
+  }
+}
+
+// void initializeDisplay()
+// {
+//   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+//   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3D (for the 128x64)
+//   display.clearDisplay();
+// }
+
+// void displayIpAddress(){
+
+//   display.clearDisplay();
+//   display.setTextSize(2);
+//   display.print(F("local IP: ")); display.println(Ethernet.localIP());
+
+//   // Serial.print("subnetMask: ");
+//   // Serial.println(Ethernet.subnetMask());
+//   // Serial.print("gatewayIP: ");
+//   // Serial.println(Ethernet.gatewayIP());
+//   // Serial.print("dnsServerIP: ");
+//   // Serial.println(Ethernet.dnsServerIP());
+// }
+
+// void displayPresets()
+// {
+//   display.setTextSize(1);
+//   display.setTextColor(WHITE);
+//   display.setCursor(0,0);
+//   display.println("Hello, world!");
+//   display.setTextColor(BLACK, WHITE); // 'inverted' text
+//   display.println(3.141592);
+//   display.setTextSize(2);
+//   display.setTextColor(WHITE);
+//   display.print("0x"); display.println(0xDEADBEEF, HEX);
+//   display.display();
+//   display.setTextSize(3);
+//   display.setTextColor(WHITE);
+//   display.println("Alarm");
+//   display.display();
+// }
+
+void loop()
+{
+}
