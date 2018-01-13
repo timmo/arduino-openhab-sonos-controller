553c6701826aa7e76266f21cc58dfc0cd86d3258
Timmo Freudl-Gierke <timmo.freudl-gierke@meteogroup.com>
2018-01-13 14:01:20 +0100

    OH2 /items REST API mock server implemented and documented

---
+++ OpenHabRestApiMockServer.py
@@ -0,0 +1,73 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from SocketServer import ThreadingMixIn
+import threading
+import argparse
+import re
+import cgi
+
+# Openhab fake items
+class Items:
+    SonosPresets = "RadioEins, Deutschlandfunk, Gute Musik, Blues"
+    ROOM_SelectedSonosPreset =''
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+
+
+    def do_GET(self):
+        if None != re.search('/rest/items/SonosPresets/state', self.path):
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(Items.SonosPresets)
+        elif None != re.search('/rest/items/ROOM_SelectedSonosPreset/state', self.path):
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(Items.ROOM_SelectedSonosPreset)
+        else:
+            self.send_response(404)
+            self.end_headers()
+        return
+
+    def do_PUT(self):
+        if None != re.search('/rest/items/ROOM_SelectedSonosPreset/state', self.path):
+            contentType = cgi.parse_header(self.headers.getheader('content-type'))
+            length = int(self.headers.getheader('content-length'))
+            Items.ROOM_SelectedSonosPreset = self.rfile.read(length)
+            self.send_response(200)
+            self.end_headers()
+            print 'ROOM_SelectedSonosPreset "%s"' % Items.ROOM_SelectedSonosPreset                
+        else:
+            self.send_response(404)
+            self.end_headers()
+        return
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+  allow_reuse_address = True
+
+  def shutdown(self):
+    self.socket.close()
+    HTTPServer.shutdown(self)
+
+class SimpleHttpServer():
+  def __init__(self, ip, port):
+    self.server = ThreadedHTTPServer((ip,port), HTTPRequestHandler)
+
+  def start(self):
+    self.server_thread = threading.Thread(target=self.server.serve_forever)
+    self.server_thread.daemon = True
+    self.server_thread.start()
+
+  def waitForThread(self):
+    self.server_thread.join()
+
+  def stop(self):
+    self.server.shutdown()
+    self.waitForThread()
+
+if __name__=='__main__':
+    server = SimpleHttpServer("127.0.0.1", 8080)
+    print 'HTTP Server Running...........'
+    server.start()
+    server.waitForThread()
+
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # arduino-openhab-sonos-controller
-A touchable (hardware) interface to control sonos speaker system via openhab2 smart home system.
+A touchable (hardware) interface to control sonos speakers via openhab2 smart home system.
+
+
+# Run Openhab2 REST API Mock Server
+
+The local runnable python3 based mock server emulated the /items resources of the Openhab2 REST API. This allows you to test your arduino code without having accdess to your OH2 server.
+
+Python3 must be installed
+
+```
+> python OpenHabRestApiMockServer.py
+```
+
+On linux use Ctrl-z and kill %1 to shutdown server.
+
+Test your mock server with curl:
+
+```
+# request presets
+> curl http://127.0.0.1:8080/rest/items/SonosPresets/state
+
+# select playlist
+> curl -X PUT -H "Content-Type: text/plain" --data-binary "Playlist 1"  http://127.0.0.1:8080/rest/items/ROOM_SelectedSonosPreset/state
+
+# check selected playlist
+> curl http://127.0.0.1:8080/rest/items/ROOM_SelectedSonosPreset/state
+```
+
+# OpenHAB 2 Setup
+
+Configure your Sonos binding in OH2 according to the documentation: https://docs.openhab.org/addons/bindings/sonos/readme.html
+
+Define the following items e.g. in file __sonos.items__
+
+```
+String SonosPresets
+String ROOM_SelectedSonosPreset
+```
+
+**ROOM_SelectedSonosPreset** contains the selected preset set by an Arduino call to the OH2 REST API. Replace ROOM with your room name.
+
+Define these rules e.g. in file __sonos.rules__
+
+```
+rule "Setup SonosPresets"
+when
+    System started
+then
+    SonosPresets.postUpdate("RadioEins, Deutschlandfunk, Gute Musik, Blues")
+end
+
+rule "React on Sonos Preset Selection - ROOM"
+when
+    ROOM_SelectedSonosPreset changed
+then
+    SonosPlay1Play.sendCommand(ROOM_SelectedSonosPreset.state)
+end
+```
+
